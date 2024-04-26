678c38d8d24f672b9617d5e759abd14cd9457d3c
c-hut <crileyprv@gmail.com>
2024-04-26 08:53:36 +0200

    Configure views in the password_checker app directory

--- password_checker/views.py
+++ password_checker/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 # Import the program from checker.py
 from .checker import evaluate_password_strength
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'password_checker.html')
 
 def check_password(request):
-    if request.method == 'POST' and request.is_ajax():
+    if request.method == 'POST' and request.headers.get('X_Requested_With') == 'XMLHttpRequest':
         password = request.POST.get('password')
         # Call password checking function
         strength = evaluate_password_strength(password)
