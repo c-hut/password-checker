b5dee3d950399d5b93630c53a05320048de04cac
c-hut <crileyprv@gmail.com>
2024-05-03 10:26:22 +0200

    Add password hashing to check_password function

--- password_checker/views.py
+++ password_checker/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from .checker import evaluate_password_strength
+from django.contrib.auth.hashers import make_password
 from .models import PasswordLogger
 
 def index(request):
@@ -11,10 +12,12 @@ def check_password(request):
         password = request.POST.get('password')
         # Call password checking function
         strength = evaluate_password_strength(password)
-
         # Log the password attempt
-        success = strength == "Strong"  # Determine if attempt was successful
-        attempt = PasswordLogger(password=password, success=success)
+        success = strength == "Strong"
+        # Hash the password before logging it
+        hashed_password = make_password(password)
+        #Save the password attempt in the database
+        attempt = PasswordLogger(password=hashed_password, success=success)
         attempt.save()
 
         return JsonResponse({'strength': strength})
