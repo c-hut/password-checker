1248df8eb154e628c1c0b931a3a96496d35ba91c
c-hut <crileyprv@gmail.com>
2024-05-03 07:53:15 +0200

    Add password logging model

--- password_checker/models.py
+++ password_checker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Log passwords
+class PasswordLogger(models.Model):
+    password = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    success = models.BooleanField()
 
-# Create your models here.
+    def __str__(self):
+        return f"Password Attempt: {self.password} - {'Success' if self.success else 'Failure'}"
